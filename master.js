d26f64a0d2e04c3b631d168cbcea78c7673d5064
Nicolás Di virgilio <nicolas_dv93@outlook.com>
2025-08-16 16:44:25 -0300

    proyecto terminado

--- master.js
+++ master.js
@@ -1,4 +1,11 @@
-function getComputerChoice(){   //function de computadora, comando aleatorio
+
+
+
+let humanScore=0;
+let computerScore=0;
+
+//function de computadora, comando aleatorio
+function getComputerChoice(){   
    let a= "piedra"
    let b= "papel"
    let c= "tijera"
@@ -6,89 +13,98 @@ function getComputerChoice(){   //function de computadora, comando aleatorio
 let i= Math.floor(Math.random() * 3)
 
 if (i === 0){
-  console.log(d[0]);
   return  d[0]
    
 } 
 if (i > 0 && i < 2){
-  console.log(d[1]);
   return   d[1]
    
 }  
 if (i > 1){
-  console.log(d[2]);
   return d[2]
   
 }  
 }
 
-/* ----------------------------------------------------- */
-function getHumanChoice() {                // function de human-choice
-   
-  let eleccion= prompt("piedra, papel ó tijera?")
-  console.log(eleccion);
-  return eleccion       
-}
+//listener de btn 
+let btnPiedra= document.querySelector("#piedra")
+btnPiedra.addEventListener("click",(e)=>{
+playRound(e.target.id)
+})
+let btnPapel= document.querySelector("#papel")
+btnPapel.addEventListener("click",(e)=>{
+playRound(e.target.id)
+})
+let btnTijera= document.querySelector("#tijera")
+btnTijera.addEventListener("click",(e)=>{
+playRound(e.target.id)
+})
 
 
-/*--------------------------------------------------------------------*/
 
-function playRound(HumanChoice, computerChoice){   //el juego en si mismo
+
+
+
+//logica del juego
+function playRound(HumanChoice){ 
+
+  const resultadosDiv = document.querySelector("#resultados");
+
+  const computerChoice=getComputerChoice()  
+
 if (HumanChoice === "papel" && computerChoice === "piedra" ) {
-  humanScore ++
-   console.log("ganaste:", humanScore )
-return   humanScore 
+   humanScore ++
+  resultadosDiv.textContent=`ganó Human ${humanScore}`
+
  } 
 
 if (HumanChoice === "piedra" && computerChoice === "tijera" ) {
- humanScore ++
-  console.log("ganaste:", humanScore )
-  return humanScore 
-
-  } 
-
-if (HumanChoice === "tijera" && computerChoice === "papel" ) {
-  humanScore ++
-  console.log("ganaste:", humanScore )
- return  humanScore 
+    humanScore ++
+  resultadosDiv.textContent=`ganó Human ${humanScore}`
 } 
 
+if (HumanChoice === "tijera" && computerChoice === "papel" ) {
+   humanScore ++
+ resultadosDiv.textContent=`ganó Human ${humanScore}`
 
+} 
 if (HumanChoice=== computerChoice) {
+    humanScore ++, computerScore ++
+  resultadosDiv.textContent=`Empate! ${humanScore} y ${computerScore} `
 
-  console.log("empate, puntos para ámbos",
-    "human: ", humanScore,
-    "machine:", computerScore) 
-   humanScore ++, computerScore ++
-return humanScore, computerScore
   }
-else {
-  console.log("ganó machine")
-  console.log("puntos:", computerScore); 
- computerScore ++
- return { human: humanScore, computer: computerScore };
-} }
+ 
+ else if (HumanChoice === "piedra" && computerChoice === "papel" ) {
+  computerChoice ++
+  resultadosDiv.textContent=`ganó machine ${computerScore}`
+  
+ } 
+else if (HumanChoice === "tijera" && computerChoice === "piedra" ) {
+  computerScore ++
+  resultadosDiv.textContent=`ganó machine ${computerScore}`
+ 
+  } 
+  
+else if (HumanChoice === "papel" && computerChoice === "tijera" ) {
+    computerScore ++
+   resultadosDiv.textContent=`ganó machine ${computerScore}`
 
- /* ---------------------------------- */
-let humanScore=0;
-let computerScore=0;
 
-const HumanChoice= getHumanChoice()//llamada a la funcion de la eleccion del jugador
-const computerChoice=getComputerChoice()//llamada a la funcion de la eleccion de la pc
+} 
 
-let resultado= playRound(HumanChoice, computerChoice) //resultado del ganador de una sola  partida
-
-
-function playGame(){
-
-for (let resultado = 0; resultado < 5; resultado++) {
-  const human = getHumanChoice();
-  const computer = getComputerChoice();
-  playRound(human, computer) 
+if (computerScore === 5) {
+  resultadosDiv.textContent=`el ganador es La máquina: ${computerScore} `
+}
+if (humanScore=== 5) {
+  resultadosDiv.textContent=`el ganador es Human ${humanScore}`
 }
 
 
 }
-playGame()
+
+
+
+
+
 
 
